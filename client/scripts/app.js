5a06cb734d8e1f97e712737b4e8ff9bce0516e06
Hack Reactor Student <student@hackreactor.com>
2018-06-22 21:22:56 -0400

    Update to state of submitHandler not being attached
    - Fetch and Send are working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,59 +1,68 @@
 // YOUR CODE HERE:
-let app = {};
-
-app.init = function() {
-  $('#main').on('click', '.username', this.handleUsernameClick);
-
-  $('#send').on('submit', '.submit', this.handleSubmit);
+let app = {
+  init: function() {
+    console.log('init');
+    $('#main').on('click', '.username', (event) => app.handleUsernameClick(event));
+  
+    $('#send').on('submit', '.submit', function(event) {
+      event.preventDefault();
+      app.handleSubmit(event);
+    });
+  },
+  
+  send: function(msg) {
+    $.ajax({
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(msg),
+      contentType: 'application/json',
+      success: (data) => {
+        console.log('chatterbox: Message sent', data);
+      },
+      error: (data) => {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  
+  fetch: function() {
+    $.ajax({
+      url: app.server,
+      type: 'GET',
+      contentType: 'application/json',
+      success: (data) => {
+        console.log('chatterbox: Messages retrieved');
+      },
+      error: (data) => {
+        console.log('chatterbox: Failed to retrieve messages');
+      }
+    });
+  },
+  
+  clearMessages: function() {
+    $('#chats').html('');
+  },
+  
+  renderMessage: function(msg) {
+    $('#chats').append(`<div><span class='username'>${ msg.username }</span>: <span class='message'>${ msg.text }</span></div>`); // TODO: escape msg here
+  },
+  
+  renderRoom: function(roomName) {
+    $('#roomSelect').append(`<option value='${roomName}'>${roomName}</option>`);
+  },
+  
+  handleUsernameClick: function(event) {
+    $(this).css('font-weight', "Bold");
+  },
+  
+  handleSubmit: function(event) {
+    console.log('handleSubmit');
+  }
 };
 
-app.send = function(msg) {
-  $.ajax({
-    url: 'http://parse.nyc.hackreactor.com/chatterbox/classes/messages',
-    type: 'POST',
-    data: JSON.stringify(msg),
-    contentType: 'application/json',
-    success: (data) => {
-      console.log('chatterbox: Message sent');
-    },
-    error: (data) => {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message', data);
-    }
-  });
-};
+$(document).ready(function() {
+  app.server = 'http://parse.nyc.hackreactor.com/chatterbox/classes/messages';
 
-app.fetch = function() {
-  $.ajax({
-    location: 'http://parse.nyc.hackreactor.com/chatterbox/classes/messages',
-    type: 'GET',
-    contentType: 'application/json',
-    success: (data) => {
-      console.log('chatterbox: Messages retrieved');
-    },
-    error: (data) => {
-      console.log('chatterbox: Failed to retrieve messages');
-    }
-  });
-};
-
-app.clearMessages = function() {
-  $('#chats').html('');
-};
-
-app.renderMessage = function(msg) {
-  $('#chats').append(`<div><span class='username'>${ msg.username }</span>: <span class='message'>${ msg.text }</span></div>`); // TODO: escape msg here
-};
-
-app.renderRoom = function(roomName) {
-  $('#roomSelect').append(`<option value='${roomName}'>${roomName}</option>`);
-};
-
-app.handleUsernameClick = function(event) {
-  console.log('usernameClick');
-};
-
-app.handleSubmit = function(event) {
-  console.log('handleSubmit');
-  event.preventDefault();
-};
+  app.init();
+});
